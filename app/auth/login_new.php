1aac06adf0514acdb6c84444a7681f56bb4c0eb2
althingsweb <ben.odhiambo@verveko.com>
2018-11-12 07:43:15 +0300

    ...

---
+++ app/auth/login_new.php
@@ -0,0 +1,19 @@
+<?php $root_path = '../../';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Login V6</title>
+</head>
+<body>
+	
+	<form action="validate_login.php" method="post">
+  		First name:<br>
+  			<input type="text" name="firstname" value=""><br>
+  		Last name:<br>
+  			<input type="text" name="lastname" value=""><br><br>
+  		<input type="submit" value="Submit">
+	</form>
+	
+  		<button href="register_new.php">Register</button>
+</body>
+</html>
---
+++ app/auth/register_new.php
@@ -0,0 +1,24 @@
+<?php $root_path = '../../';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Login V6</title>
+</head>
+<body>
+	
+	<form action="validate_register.php">
+  		First name:<br>
+  			<input type="text" name="firstname" value=""><br>
+  		Last name:<br>
+  			<input type="text" name="lastname" value=""><br><br>
+  		User name:<br>
+  			<input type="text" name="username" value=""><br><br>
+  		Password:<br>
+  			<input type="password" name="password" value=""><br>
+  		Confirm Password:<br>
+  			<input type="password" name="confirmpassword" value=""><br><br>
+  			
+  		<input type="submit" value="Submit">
+	</form>
+</body>
+</html>
